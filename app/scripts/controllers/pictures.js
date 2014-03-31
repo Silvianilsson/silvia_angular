a7c082552ec5784186ad46742927f73fcfb9df90
Silvia Nilsson <silvia.nilsson@hotmail.com>
2014-03-31 21:51:47 +0200

    ändrade carousel

--- app/scripts/controllers/pictures.js
+++ app/scripts/controllers/pictures.js
@@ -2,38 +2,9 @@
 
 angular.module('silviaApp')
   .controller('PicturesCtrl', function ($scope) {
-    $scope.myInterval = 5000;
-    var slides = $scope.slides = [];
-
-    slides.push({
-      image: 'images/solros.JPG',
-      text: ['More', 'Extra', 'Lots of', 'Surplus'][slides.length % 4] + ' ' + ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-    });
-
-    slides.push({
-      image: 'images/sille.JPG',
-      text: ['More', 'Extra', 'Lots of', 'Surplus'][slides.length % 4] + ' ' + ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-    });
-
-    slides.push({
-      image: 'images/sille2.JPG',
-      text: ['More', 'Extra', 'Lots of', 'Surplus'][slides.length % 4] + ' ' + ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-    });
-
-    slides.push({
-      image: 'images/helikopter.JPG',
-      text: ['More', 'Extra', 'Lots of', 'Surplus'][slides.length % 4] + ' ' + ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-    });
-
-    /*
-    $scope.addSlide = function () {
-      var newWidth = 600 + slides.length;
-      slides.push({
-        image: 'http://placekitten.com/' + newWidth + '/300',
-        text: ['More', 'Extra', 'Lots of', 'Surplus'][slides.length % 4] + ' ' + ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-      });
-    };
-    for (var i = 0; i < 4; i++) {
-      $scope.addSlide();
-    }*/
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
   });
